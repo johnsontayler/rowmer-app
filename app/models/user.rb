e368b8fa9a47b03ba906a690d611a876943d59f6
Tayler Johnson <johnsontayler@me.com>
2019-10-20 15:52:31 -0300

    owner, tenant, agent <> room relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+  #buyer
   has_many :journeys
+
+  #owner, tenant, agent
+  has_many :rooms, foreign_key: :owner_id
+  has_many :rooms, foreign_key: :tenant_id
+  has_many :rooms, foreign_key: :agent_id
 end
